6abdc56e60a79ff9458efdbc50262307e286768c
Pandu Rijal Pasa <pandurijal96@gmail.com>
2019-01-30 12:28:14 +0700

    Updates

---
+++ precache-manifest.fb00e533d9e1f4cc5a575ca526241e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd41bb22c058aa0e26b7",
+    "url": "/todolite/static/js/runtime~main.bd41bb22.js"
+  },
+  {
+    "revision": "7110e6095f3ed727912c",
+    "url": "/todolite/static/js/main.7110e609.chunk.js"
+  },
+  {
+    "revision": "2f075dc3fd9f513f14f0",
+    "url": "/todolite/static/js/1.2f075dc3.chunk.js"
+  },
+  {
+    "revision": "7110e6095f3ed727912c",
+    "url": "/todolite/static/css/main.20c6816c.chunk.css"
+  },
+  {
+    "revision": "2f075dc3fd9f513f14f0",
+    "url": "/todolite/static/css/1.545ec593.chunk.css"
+  },
+  {
+    "revision": "5b9792f6aa6167fb3b996e17d282145c",
+    "url": "/todolite/index.html"
+  }
+];
